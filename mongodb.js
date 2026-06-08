4b90dd3a8c8f157632ae18d4853c49aadc2cb992
Boorchibat <coolboyboorchi@gmail.com>
2026-06-08 07:57:38 -0400

    fix

--- mongodb.js
+++ mongodb.js
@@ -17,10 +17,6 @@ const project = require("./src/route/ProjectRoute");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-  console.log(req.method, req.url);
-  next();
-});
 
 app.use(
   cors({
@@ -33,6 +29,11 @@ app.use(
 );
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log("REQUEST:", req.method, req.originalUrl);
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
@@ -73,6 +74,10 @@ async function startServer() {
     console.error(err);
     process.exit(1);
   }
+  app.use((req, res) => {
+  console.log("NO ROUTE MATCHED:", req.method, req.originalUrl);
+  res.status(404).json({ message: "Not found" });
+});
 }
 
 startServer();
